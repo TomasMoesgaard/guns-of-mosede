11343d5c82ebec04e13899208de4f3424ae158fc
unknown <tmoesg11@AAU103718.student.aau.dk>
2016-06-17 11:41:25 +0200

    corrected flags and controls panel

---
+++ Cannon Prototype/Assets/CameraChange.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraChange : MonoBehaviour {
+
+    public SteamVR_GameView GameView;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ChangeCamera();
+        }
+
+	
+	}
+
+    public void ChangeCamera()
+    {
+
+        if (GameView.enabled)
+        {
+
+            GameView.enabled = false;
+
+        }
+        else
+        {
+
+            GameView.enabled = true;
+
+        }
+
+    }
+}
